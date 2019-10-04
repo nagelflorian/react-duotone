fc32e048b3be451780038b42f7a4d365fccc19f3
Florian Nagel <hi@floriannagel.xyz>
2019-10-04 16:41:02 +0200

    Remove unused import

--- examples/image/app.js
+++ examples/image/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { DuotoneImage } from '../../src/main.js';
 import image1 from './image_1.jpg';
